4cbabc90cc986aa431338663f0ee79aa7763ebf1
kotba3ilio <31704436+kotba3ilio@users.noreply.github.com>
2025-01-20 02:11:59 +0000

    Deploying to gh-pages from @ kotba3ilio/goit-js-hw-11@49ade59ee3f443ff06d5a3118e8816c3ccafbd05 🚀

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-import{i as c,S as l}from"./assets/vendor-B07T6_gy.js";(function(){const a=document.createElement("link").relList;if(a&&a.supports&&a.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const n of r.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&o(n)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function o(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();const u=t=>{const a={method:"get"},s=new URLSearchParams({key:"48321806-d480a24496502db5d22ba5dda",image_type:"photo",orientation:"hotizontal",safesearch:!0,q:t});return fetch(`https://pixabay.com/api/?${s.toString()}`,a).then(o=>{if(!o.ok)throw new Error(o.status);return o.json()}).catch(o=>{c.show({color:"#ffafb4",message:o.message})})},f=new l(".gallery a",{captionsData:"alt",captionDelay:250});function d(t){return`<li><a class="gallery-link" href="${t.largeImageURL}"><img class="gallery-image" src="${t.previewURL}" alt="${t.tags}"/></a></li>`}function m(t,a){if(a.length==0)throw new Error("Sorry, there are no images matching your search query. Please try again!");const s=a.map(o=>d(o)).join("");t.innerHTML=s,f.refresh()}const h=document.querySelector("#search"),i=document.querySelector(".gallery");function g(){return'<div class="loader"></div>'}document.querySelector(".submit").addEventListener("click",t=>{t.preventDefault(),i.innerHTML=g(),u(h.value).then(s=>{setTimeout(()=>{m(i,s.hits)},300)}).catch(s=>{c.error({title:"Error",message:s.message})})});
+import{i as n,S as u}from"./assets/vendor-B07T6_gy.js";(function(){const i=document.createElement("link").relList;if(i&&i.supports&&i.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))a(t);new MutationObserver(t=>{for(const r of t)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&a(o)}).observe(document,{childList:!0,subtree:!0});function s(t){const r={};return t.integrity&&(r.integrity=t.integrity),t.referrerPolicy&&(r.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?r.credentials="include":t.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function a(t){if(t.ep)return;t.ep=!0;const r=s(t);fetch(t.href,r)}})();const d=e=>{const i={method:"get"},s=new URLSearchParams({key:"48321806-d480a24496502db5d22ba5dda",image_type:"photo",orientation:"hotizontal",safesearch:!0,q:e});return fetch(`https://pixabay.com/api/?${s.toString()}`,i).then(a=>{if(!a.ok)throw a.status;return a.json()}).catch(a=>{n.error({title:"Error",message:a})})},h=new u(".gallery a",{captionsData:"alt",captionDelay:250}),l=document.querySelector(".gallery");function p(e){return`<li>
+  <a class="gallery-link" href="${e.largeImageURL}">
+  <img class="gallery-image" src="${e.previewURL}" alt="${e.tags}"/>
+  <ul class="statistic">
+  <li>
+  <h3 class="statistic-title">Likes</h3>
+  <p class="statistic-text">${e.likes}</p>
+  </li>
+  <li>
+  <h3 class="statistic-title">Views</h3>
+  <p class="statistic-text">${e.views}</p>
+  </li>
+  <li>
+  <h3 class="statistic-title">Comments</h3>
+  <p class="statistic-text">${e.comments}</p>
+  </li>
+  <li>
+  <h3 class="statistic-title"> Downloads</h3>
+  <p class="statistic-text">${e.downloads}</p>
+  </li>
+  </ul>
+  </a>
+  </li>`}function f(e){if(l.innerHTML="",e.length==0)throw"Sorry, there are no images matching your search query. Please try again!";const i=e.map(s=>p(s)).join("");l.innerHTML=i,h.refresh()}const y=document.querySelector("#search"),c=document.querySelector(".loader-wrapper");document.querySelector(".submit").addEventListener("click",e=>{e.preventDefault(),c.style.display="block",d(y.value).then(s=>{f(s.hits)}).catch(s=>{n.error({title:"Error",message:s})}).finally(()=>{c.style.display="none"})});
 //# sourceMappingURL=index.js.map
